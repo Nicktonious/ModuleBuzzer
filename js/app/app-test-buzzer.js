da2f1af7c0b22816268f316d35aa5938f1715f43
Ivan Kukushkin <github-sync@outlook.com>
2023-03-01 12:45:46 +0400

    refactor: создана минифицированая версия файла

--- js/app/app-test-buzzer.js
+++ js/app/app-test-buzzer.js
@@ -1,5 +1,5 @@
-const err = require('https://raw.githubusercontent.com/konstantin-ki/Physics-heat-capacity/ver2/js/module/ModuleAppError.min.js');
-const NumIs = require('https://raw.githubusercontent.com/konstantin-ki/Physics-heat-capacity/ver2/js/module/ModuleAppMath.min.js');
+const err = require('https://raw.githubusercontent.com/Konkery/ModuleAppError/main/js/module/ModuleAppError.min.js');
+const NumIs = require('https://raw.githubusercontent.com/Konkery/ModuleAppMath/main/js/module/ModuleAppMath.min.js');
      NumIs.is(); //добавить функцию проверки целочисленных чисел в Number
 
 const buzzer_class = require('https://raw.githubusercontent.com/konstantin-ki/Physics-heat-capacity/ver2/js/module/ModuleBuzzer2.min.js');
