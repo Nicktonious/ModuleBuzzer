ee37ed7ae0cd075daae0d4576045e680faa615e0
Nikita <nikita.umnov@mas-tech.ru>
2024-03-31 22:11:27 +0400

    feat: реализация базовых методов On(), Off() feat: реализация базовых тасков PlaySound, BeepOnce, BeepTwice doc: обновлена документация

--- README.md
+++ README.md
@@ -1,2 +1,154 @@
-# ModuleBuzzer
-The Espruino module supports piezoelectric emitter
+<div style = "font-family: 'Open Sans', sans-serif; font-size: 16px">
+
+# ModuleServo
+
+<div style = "color: #555">
+    <p align="center">
+    <img src="./res/logo.png" width="400" title="hover text">
+    </p>
+</div>
+
+## Лицензия
+
+<div style = "color: #555">
+
+В разработке
+</div>
+
+## Описание
+<div style = "color: #555">
+
+Модуль предназначен для прикладной работы пьезозуммерами в рамках фреймворка EcoLight и обеспечивает следующий функционал:
+- Инициализацию и идентификацию различных моделей пьезозуммеров в соответствии с их характеристиками;
+- Включение пьезозуммера с заданной частотой;
+- Генерация различных звуковых паттернов и их проигрыш посредством выполнения тасков.
+
+Модуль разработан в соответствии с [архитектурой актуаторов](https://github.com/Konkery/ModuleActuator/blob/main/README.md), соответственно, *ClassServo* наследует и реализует является функционал *ClassMiddleActuator*, а прикладная работа с данным модулем выполняется через *ClassChannelActuator*, который обеспечивает унифицированный интерфейс.
+
+</div>
+
+## Конструктор
+<div style = "color: #555">
+
+Для создания объекта **Buzzer** требуется указание в конфиге его метаданных, используемого пина, а так же минимальной и максимальной частоты на которой предполагается его использовать. 
+Пример конфигурации:
+```json
+...
+"08": {
+    "pins": ["A3"],
+    "name": "Buzzer",
+    "minFreq": 400,
+    "maxFreq": 3000,
+    "article": "02-501-0204-000-0001",
+    "type": "actuator",
+    "channelNames": ["freq"],
+    "typeInSignal": ["analog"],
+    "quantityChannel": 1,
+    "busTypes": [],
+    "manufacturingData": {}
+}
+...
+```
+
+</div>
+
+### Поля
+<div style = "color: #555">
+
+- <mark style="background-color: lightblue">_MinFreq</mark> - минимальная частота звучания;
+- <mark style="background-color: lightblue">_MaxFreq</mark> - максимальная частота звучания.
+</div>
+
+### Методы
+<div style = "color: #555">
+
+- <mark style="background-color: lightblue">On(_chNum, _val)</mark> - запускает звучание на заданной частоте (0 ~ minFreq, 1 ~ maxFreq). При работе через канал, аргумент *_chNum* пропускается;
+- <mark style="background-color: lightblue">Off()</mark> - останавливает звучание.
+
+</div>
+
+### Примеры
+#### Инициализация и запуск пьезо-зуммера
+<div style = "color: #555">
+
+```js
+//Инициализация 
+const bz = SensorManager.CreateSensor('08')[0];
+//Запуск работы зуммера с частотой 600 Гц
+bz.On(600);
+//Запуск с другой частотой через 1 сек
+setTimeout(() => { 
+    bz.On(1000);    
+}, 1500);
+//Прекращение работы
+setTimeout(() => { 
+    bz.Off(); 
+}, 3000);
+```
+
+</div>
+
+#### Запуск цепочки тасков
+<div style = "color: #555">
+
+```js
+//Вызов одного пика через основной, универсальный таск 
+bz.RunTask('PlaySound', { freq: 300, numRep: 1, prop: 0.5, pulseDur: 800 });  
+.then(
+    // Вызов пика через таск, принимающий частоту и длину импульса 
+    () => bz.RunTask('BeepOnce', 500, 800);
+).then(
+    // вызов двойного звукового сигнала
+    () => bz.RunTask('BeepTwice', 800, 500);                   
+).then(
+    () => { console.log('Done!'); }
+);
+```
+
+</div>
+
+#### Добавление нового таска
+<div style = "color: #555">
+
+```js
+//Объявление элементарного таска, запускающего зуммер на 3 сек
+bz.AddTask('Beep3sec', (freq) => {
+    this.On(freq);
+    setTimeout(() => {
+        this.Off();
+        //Завершение выполнения таска
+        this.ResolveTask(0);
+    }, 3000);
+});
+
+bz.RunTask('Beep3sec', 500);
+    .then(() => print(`Done after 3 sec!`));
+```
+
+</div>
+
+#### Отмена выполнения таска после его вызова 
+<div style = "color: #555">
+
+```js
+bz.RunTask('BeepTwice', 500, 1200);
+
+setTimeout(() => {
+    bz.CancelTask();
+}, 1000);
+```
+
+</div>
+
+#### Результат выполнения:
+
+<div align='left'>
+    <img src="" alt="Image not found">
+</div>
+
+### Зависимости
+<div style = "color: #555">
+
+</div>
+
+</div>
--- js/module/ModuleBuzzer.js
+++ js/module/ModuleBuzzer.js
@@ -1,100 +1,35 @@
 /**
- * @class
- * Класс ClassBuzzerTypePlay обеспечивает прикладной тип для методов класса ClassBuzzer.
- * Пример набора аргументов конструктора:
- * {4 /импульса/, 200 /ms длительность/, 0.5 /50% скважность/}
- */
-class ClassBuzzerTypePlay {
-    /**
-     * @constructor
-     * @param {number} _pulseDur    - длительность звучания в ms [50<=x<infinity]
-     * @param {number} _numRep      - количество повторений [1...n]
-     * @param {number} _freq        - рабочая частота buzzer
-     * @param {number} _prop        - пропорция ЗВУК/ТИШИНА на одном периоде [0<x<=1]
-     */
-    constructor(_pulseDur, _numRep, _freq, _prop) {
-        this.name = 'ClassBuzzerTypePlay'; //переопределяем имя типа
-        this._PulseDur = undefined;
-        this._NumRep = undefined;
-        this._Freq = undefined;
-        this._Proportions = undefined;
-        
-        this.Init(_pulseDur, _numRep, _freq, _prop); //инициализировать поля
-    }
-    /*******************************************CONST********************************************/
-    /**
-     * @const
-     * @type {number}
-     * Константа ERROR_CODE_ARG_VALUE определяет код ошибки, которая может произойти
-     * в случае передачи в конструктор не валидных данных
-     */
-    static get ERROR_CODE_ARG_VALUE() { return 10; }
-    /**
-     * @const
-     * @type {string}
-     * Константа ERROR_MSG_ARG_VALUE определяет сообщение ошибки, которая может произойти
-     * в случае передачи в конструктор не валидных данных
-     */
-    static get ERROR_MSG_ARG_VALUE() { return `ERROR>> invalid data. ClassID: ${this.name}`; }
-    /*******************************************END CONST****************************************/
-    /**
-     * Метод инициализирует поля объекта
-     */
-    Init(_pulseDur, _numRep, _freq, _prop) {
-        let pulsedur = _pulseDur || 100;
-        let numrep = _numRep || 1;
-        let freq = _freq || 4000;
-        let prop = _prop || 0.5;
-        
-        /*проверить переданные аргументы  на валидность*/
-        if (!(typeof (pulsedur) === 'number')   ||
-            !(typeof (numrep) === 'number')     ||
-            !(typeof (freq) === 'number')       ||
-            !(typeof (prop) === 'number')       ||
-            !(Number.isInteger(pulsedur))       ||
-            !(Number.isInteger(numrep))         ||
-            !(Number.isInteger(freq))) {
-                
-                throw new err(ClassTypeBuzzerPlay.ERROR_MSG_ARG_VALUE,
-                              ClassTypeBuzzerPlay.ERROR_CODE_ARG_VALUE);
-        }
-        if (pulsedur<50) {pulsedur=50;} //нормализовать значение длительности импульса звучания
-        if (prop<0 || prop>1) {prop=0.5;}
-        
-        /*инициализировать поля*/
-        this._PulseDur = pulsedur;
-        this._NumRep = numrep;
-        this._Freq = freq;
-        this._Proportions = prop;
-    }
-}
-
+ * @typedef TypeBuzzerStart
+ * @property {Number} freq     - частота
+ * @property {Number} numRep   - количество повторений [1...n]
+ * @property {Number} pulseDur - длительность звучания в ms [50<=x<infinity]
+ * @property {Number} prop     - пропорция ЗВУК/ТИШИНА на одном периоде [0<x<=1]
+*/  
 /**
  * @class
  * Класс ClassBuzzer реализует логику работы пьезодатчика.
- * Для работы класса требуется подключить модуль ModuleAppMath, где 
- * добавляется функция проверки на целочисленность
  */
-class ClassBuzzer {
+class ClassBuzzer extends ClassActuator {
+    /**
+     * @typedef BuzzerOptsType
+     * @property {[Pin]} pins - массив с одним пином
+     * @property {number} maxFreq - минимальная частота на которой пьезо будет издавать звук
+     * @property {number} minFreq
+     */
     /**
      * @constructor
-     * @param {Object} _opt   - объект класса Pin
+     * @param {BuzzerOptsType} _opts 
      */
-    constructor(_opt) {
-        this.name = 'ClassBuzzer'; //переопределяем имя типа
+    constructor(_opts, _actuatorProps) {
+        this.name = 'ClassBuzzer';                                  //переопределение имени класса
+        ClassActuator.apply(this, [_opts, _actuatorProps]);   //вызов родительского конструктора
+        this._MinFreq = _opts.minFreq;
+        this._MaxFreq = _opts.maxFreq;
 
-        /*проверить переданные аргументы на валидность*/
-        if ((typeof (_opt) === 'undefined')) {
-            
-            throw new err(ClassTypeBuzzer.ERROR_MSG_ARG_VALUE ,
-                          ClassTypeBuzzer.ERROR_CODE_ARG_VALUE);
-        }
-        if(!(_opt instanceof Pin)) {
-            
-            throw new err(ClassTypeBuzzer.ERROR_MSG_ARG_VALUE,
-                          ClassTypeBuzzer.ERROR_CODE_ARG_VALUE);
-        }
-        this._BuzPin = _opt; 
+        if (typeof this._MaxFreq !== 'number' || 
+            typeof this._MinFreq !== 'number') throw new Error('Invalid range values'); 
+
+        this.InitTasks();
     }
     /*******************************************CONST********************************************/
     /**
@@ -111,40 +46,94 @@ class ClassBuzzer {
      * в случае передачи не валидных данных
      */
     static get ERROR_MSG_ARG_VALUE() { return `ERROR>> invalid data. ClassID: ${this.name}`; }
-    /*******************************************END CONST****************************************/
+    /*******************************************END CONST*************************************** */
     /**
-     * @method
-     * 
-     * @param {Object} _opt   - объект класса ClassTypeBuzzerPlay
-     */
-    PlayBeep(_opt) {
-        /*проверить переданные аргументы на валидность*/
-        if(!(_opt instanceof ClassBuzzerTypePlay)) {
-            throw new ClassAppError(ClassBuzzer.ERROR_MSG_ARG_VALUE,
-                                    ClassBuzzer.ERROR_CODE_ARG_VALUE);
-        }        
-        /*-сформировать двойной звуковой сигнал */
-        let Thi = _opt._PulseDur; //длительность звукового сигнала
-        let Tlo = Math.floor(_opt._PulseDur*(1-_opt._Proportions)/_opt._Proportions); //длительность паузы
-        let beep_count = _opt._NumRep*2; //количество полупериодов(!) звукового сигнала
-        let beep_flag = true;
-        analogWrite(this._BuzPin, 0.5, { freq : _opt._Freq }); //включить звуковой сигнал
-        let beep_func = ()=>{
-            --beep_count;
-            if (beep_count > 0) {
-                if (beep_flag) {
-                    digitalWrite(this._BuzPin, beep_flag); //выключить звук
-                        setTimeout(beep_func, Tlo); //взвести setTimeout
+    * @method
+    * Инициализирует стандартные таски модуля
+    */
+    InitTasks() {
+        this._Channels[0].AddTask('PlaySound', (opts) => {
+            // console.log(this);
+            //проверка и валидация аргументов 
+            ['freq', 'numRep', 'pulseDur', 'prop'].forEach(property => {
+                if (typeof opts[property] !== 'number' || opts[property] < 0) throw new Error('Invalid args');
+            });
+            opts.prop = E.clip(opts.prop, 0, 1); 
+            opts.pulseDur = E.clip(opts.pulseDur, 0, 2147483647);  //ограничение длины импульса максимальным знчением, которое может быть передано в setTimeout
+
+            /*-сформировать двойной звуковой сигнал */
+            const freq = opts.freq;
+            let Thi = opts.pulseDur; //длительность звукового сигнала
+            let Tlo = Math.floor(opts.pulseDur*(1 - opts.prop)/opts.prop); //длительность паузы
+            count = opts.numRep*2;                                     //количество полупериодов(!) звукового сигнала
+            let beep_flag = true;
+
+            let beep_func = () => {
+                --count;
+                if (count > 0) {
+                    if (beep_flag) {
+                        this.Off();                                           //выключить звук
+                        this._Interval = setTimeout(beep_func, Tlo);          //взвести setTimeout
+                    } else {
+                        this.On(freq);                                     //включить звук
+                        this._Interval = setTimeout(beep_func, Thi);          //взвести setTimeout
+                    }
+                    beep_flag = !beep_flag;
                 } else {
-                    analogWrite(this._BuzPin, 0.5, {freq: _opt._Freq}); //включить звук
-                        setTimeout(beep_func, Thi); //взвести setTimeout
-                }
-                beep_flag = !beep_flag;
-            }
+                    this.ResolveTask(0);               //завершение таска
+                };
+            };
+
+            this.On(freq) //включить звуковой сигнал
+            this._Interval = setTimeout(beep_func, Thi);
+        });
+
+        this._Channels[0].AddTask('BeepOnce', function(freq, dur) {
+            if (!Number.isInteger(dur) || dur < 0) throw new Error('Invalid args');
+
+            this.On(freq);
+            setTimeout(() => {
+                this.Off();
+                this.ResolveTask(0);
+            }, dur);
+        });    
+
+        this._Channels[0].AddTask('BeepTwice', (freq, dur) => {
+            // console.log(freq, dur);
+            if (!Number.isInteger(dur) || dur < 0) throw new Error('Invalid args');
+        
+            this.On(freq);          //вкл звук 
+
+            setTimeout(() => {
+                this.Off();
+            }, dur);                //выкл звук через один полупериод
+
+            setTimeout(() => {
+                this.On(freq);
+            }, dur*2);              //вкл звук через два полупериода
+
+            setTimeout(() => {
+                this.Off();         //выкл через 3 полупериода
+                setTimeout(() => { this.ResolveTask(0); }, dur*4);  //деактивировать таск через 2 полных периода
+            }, dur*3);
+        });
+    }
+    //_val - коэффициент от 0 до 1
+    On(_chNum, _val) {
+        if (typeof _val !== 'number') throw new Error();
+        _val = E.clip(_val, 0, 1);
+        const proportion = (x, in_min, in_max, out_min, out_max) => {
+            return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
         }
-        setTimeout(beep_func, Thi);
+        if (this._IsChOn[_chNum]) this.Off();
+        let freq = proportion(_val, 0, 1, this._MinFreq, this._MaxFreq);
+        analogWrite(this._Pins[0], 0.5, { freq : freq }); //включить звуковой сигнал
+    }
+
+    Off() {
+        digitalWrite(this._Pins[0], 1);
+        this._IsChOn[0] = false;
     }
 }
 
-exports = { ClassBuzzerTypePlay:    ClassBuzzerTypePlay,
-            ClassBuzzer:            ClassBuzzer};
+exports = ClassBuzzer;
